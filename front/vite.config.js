8e5a913bb9bbb68320d179d2907cce212d75b724
jason.w <jasonw.lab@gmail.com>
2025-11-19 19:24:50 +0900

    フロントdeploy整備

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,10 +1,32 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 5173,
+const pathSrc = fileURLToPath(new URL('./src', import.meta.url))
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    base: '/ec-demo/',
+    resolve: {
+      alias: {
+        "@": pathSrc,
+      },
+    },
+    plugins: [vue()],
+    server: {
+      host: '0.0.0.0',
+      port: +env.VITE_APP_PORT,
+      open: true,
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          changeOrigin: true,
+          target: env.VITE_APP_API_URL,
+          rewrite: (path) =>
+            path.replace(new RegExp('^' + env.VITE_APP_BASE_API), ''),
+        },
+      },
+    },
   }
 })
